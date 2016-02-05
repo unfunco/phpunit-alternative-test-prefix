892d93103429707e896b203a0fa9ec44f75dde07
Daniel Morris <daniel@honestempire.com>
2016-02-05 23:32:54 +0000

    Further refactoring and adding license headers

--- src/Listener/AlternativeTestPrefixListener.php
+++ src/Listener/AlternativeTestPrefixListener.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * PHPUnit alternative test prefix listener
+ *
+ * Copyright © 2016 Daniel Morris <daniel@honestempire.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 namespace Unfunco\PHPUnit\Listener;
 
 use Exception;
@@ -21,18 +45,18 @@ use ReflectionMethod;
 class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
 {
     /**
+     * The prefixes specified within the PHPUnit configuration file.
+     *
+     * @var array
+     */
+    protected $prefixes;
+
+    /**
      * The regular expression used to check for configured test prefixes.
      *
      * @var string
      */
     protected $prefixRegExp;
-
-    /**
-     * The prefixes specified within the PHPUnit configuration file.
-     *
-     * @var array
-     */
-    private $prefixes;
 
     /**
      * Instantiates a new instance of the alternative test prefix listener.
@@ -61,14 +85,10 @@ class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
             }
 
             $reflectedClass = new ReflectionClass($testClass);
-            $reflectedClassMethods = $reflectedClass->getMethods(ReflectionMethod::IS_PUBLIC);
+            $testMethodNames = $this->getTestMethodNames($reflectedClass);
 
-            foreach ($reflectedClassMethods as $reflectedClassMethod) {
-                $reflectedClassMethodName = $reflectedClassMethod->getName();
-
-                if (1 === preg_match($this->prefixRegExp, $reflectedClassMethodName)) {
-                    $this->addTestToSuite($suite, $reflectedClass, $reflectedClassMethodName);
-                }
+            foreach ($testMethodNames as $testMethodName) {
+                $this->addTestToSuite($suite, $reflectedClass, $testMethodName);
             }
         }
     }
@@ -78,7 +98,7 @@ class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
      *
      * @param PHPUnit_Framework_Test $test The test.
      * @param Exception              $e    The exception.
-     * @param float                   $time The time.
+     * @param float                  $time The time.
      */
     public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
@@ -89,7 +109,7 @@ class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
      *
      * @param PHPUnit_Framework_Test                 $test The test.
      * @param PHPUnit_Framework_AssertionFailedError $e    The error.
-     * @param float                                   $time The time.
+     * @param float                                  $time The time.
      */
     public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
     {
@@ -100,7 +120,7 @@ class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
      *
      * @param PHPUnit_Framework_Test $test The test.
      * @param Exception              $e    The exception.
-     * @param float                   $time The time.
+     * @param float                  $time The time.
      */
     public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
@@ -111,7 +131,7 @@ class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
      *
      * @param PHPUnit_Framework_Test $test The test.
      * @param Exception              $e    The exception.
-     * @param float                   $time The time.
+     * @param float                  $time The time.
      */
     public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
@@ -122,7 +142,7 @@ class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
      *
      * @param PHPUnit_Framework_Test $test The test.
      * @param Exception              $e    The exception.
-     * @param float                   $time The time.
+     * @param float                  $time The time.
      */
     public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
@@ -150,7 +170,7 @@ class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
      * {@inheritDoc}
      *
      * @param PHPUnit_Framework_Test $test The test.
-     * @param float                   $time The time.
+     * @param float                  $time The time.
      */
     public function endTest(PHPUnit_Framework_Test $test, $time)
     {
@@ -174,5 +194,28 @@ class AlternativeTestPrefixListener implements \PHPUnit_Framework_TestListener
         }
 
         $suite->addTest($test, PHPUnit_Util_Test::getGroups($reflectedClass->getName(), $methodName));
+    }
+
+    /**
+     * Returns an array of test method names from a ReflectionClass.
+     *
+     * @param ReflectionClass $reflectedClass The reflected class.
+     *
+     * @return array
+     */
+    protected function getTestMethodNames(ReflectionClass $reflectedClass)
+    {
+        $testMethodNames = [];
+        $reflectedClassMethods = $reflectedClass->getMethods(ReflectionMethod::IS_PUBLIC);
+
+        foreach ($reflectedClassMethods as $reflectedClassMethod) {
+            $reflectedClassMethodName = $reflectedClassMethod->getName();
+
+            if (1 === preg_match($this->prefixRegExp, $reflectedClassMethodName)) {
+                $testMethodNames[] = $reflectedClassMethodName;
+            }
+        }
+
+        return $testMethodNames;
     }
 }
